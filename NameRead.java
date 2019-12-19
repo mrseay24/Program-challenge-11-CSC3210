044489a9e3082db6cd1aa98c30bb114d71645a06
mrseay24 <sidneyeseay@gmail.com>
2019-12-18 20:32:49 -0500

    Add files via upload

---
+++ NameRead.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class NameRead {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = "";
+		System.out.print("Enter your first name, so it can print on screen: ");
+		sc = new Scanner(System.in);
+		str = sc.nextLine();
+		System.out.print(str);
+	}
+}
